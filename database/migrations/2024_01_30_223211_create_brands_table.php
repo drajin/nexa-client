3786a3de3891f716215db739b0dcaf883e1e85b4
Dejan Rajin <drajin@gmail.com>
2024-02-04 22:06:14 +0100

    change logo field from text to binary

--- database/migrations/2024_01_30_223211_create_brands_table.php
+++ database/migrations/2024_01_30_223211_create_brands_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
             $table->uuid('id')->primary();
             $table->string('name');
             $table->text('description');
-            $table->text('logo_base64');
+            $table->binary('logo');
             $table->string('status');
             $table->timestamps();
             $table->softDeletes();
