0bc0e0ebf57a7b05590949b9e026c465fb858843
Dejan Rajin <drajin@gmail.com>
2024-02-04 21:56:19 +0100

    add casts to gender model

--- app/Models/Gender.php
+++ app/Models/Gender.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Enums\Gender as GenderEnum;
 
 class Gender extends Model
 {
     use HasFactory, HasUuids;
+
+    protected $casts = [
+        'type' => GenderEnum::class,
+    ];
 
     public function customer(): HasOne
     {
