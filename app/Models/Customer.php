8b818beca4c22ce231e86836ecdf75c10a317c3b
Dejan Rajin <drajin@gmail.com>
2024-03-10 22:59:46 +0100

    feat: add gender enum cast to customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\Gender as GenderEnum;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +16,7 @@ class Customer extends Model
 
     protected $casts = [
         'birthday' => 'date',
+        'type' => GenderEnum::class,
     ];
 
     public function orders(): HasMany
