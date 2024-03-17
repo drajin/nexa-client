ff3bbd7c65fd46cbd1079dd469b59f1b2f241592
Dejan Rajin <drajin@gmail.com>
2024-03-17 02:11:40 +0100

    feat: add rel customer for brand

---
+++ app/Filament/Resources/CustomerResource/RelationManagers/AddressRelationManager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Resources\CustomerResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AddressRelationManager extends RelationManager
+{
+    protected static string $relationship = 'address';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('street')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('street_number')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('postcode')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('city')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('country')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('post_box')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('street')
+            ->columns([
+                Tables\Columns\TextColumn::make('street'),
+                Tables\Columns\TextColumn::make('street_number')
+                    ->label('Number'),
+                Tables\Columns\TextColumn::make('postcode'),
+                Tables\Columns\TextColumn::make('city'),
+                Tables\Columns\TextColumn::make('country'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make()
+                    ->createAnother(false),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
