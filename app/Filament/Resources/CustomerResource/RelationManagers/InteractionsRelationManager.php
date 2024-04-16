0a3bf8658c20092bd7f692f42cb640ec492b41e1
Dejan Rajin <drajin@gmail.com>
2024-04-17 00:31:04 +0200

    feat: add interactions rel manager

---
+++ app/Filament/Resources/CustomerResource/RelationManagers/InteractionsRelationManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources\CustomerResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class InteractionsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'interactions';
+
+    public function isReadOnly(): bool
+    {
+        return false;
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('customer_id')
+                    ->relationship('customer', 'id')
+                    ->searchable()
+                    ->required(),
+                Forms\Components\Select::make('note_id')
+                    ->relationship('note', 'title'),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('note.title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make()
+                    ->createAnother(false),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
